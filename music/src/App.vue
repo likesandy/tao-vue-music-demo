382f09220fd72483a64729db45f96febb872624d
sandy <1445709137@qq.com>
2021-07-08 23:04:05 +0800

    first commit

---
+++ music/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MainHeader></MainHeader>
+    <Tabbar></Tabbar>
+    <keep-alive include="Singer,Search,Recommend,Account">
+      <router-view></router-view>
+    </keep-alive>
+    <Player></Player>
+  </div>
+</template>
+<script>
+import MainHeader from './components/MainHeader'
+import Tabbar from './components/Tabbar'
+import Player from './views/Player'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+    Tabbar,
+    Player
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
---
+++ music/vue-meta-info.js
@@ -0,0 +1,80 @@
+export default {
+  recommend: {
+    title: '我是推荐界面',
+    meta: [
+      {
+        name: 'keyWords',
+        content: '关键字2,关键字2,关键字2'
+      },
+      {
+        name: 'description',
+        content: '这是一段网页的描述2'
+      }
+    ]
+  },
+  singer: {
+    title: '我是歌手界面',
+    meta: [
+      {
+        name: 'keyWords',
+        content: '关键字2,关键字2,关键字2'
+      },
+      {
+        name: 'description',
+        content: '这是一段网页的描述2'
+      }
+    ]
+  },
+  rank: {
+    title: '我是rank',
+    meta: [
+      {
+        name: 'keyWords',
+        content: '关键字3,关键字3,关键字3'
+      },
+      {
+        name: 'description',
+        content: '这是一段网页的描述3'
+      }
+    ]
+  },
+  search: {
+    title: '我是搜索界面',
+    meta: [
+      {
+        name: 'keyWords',
+        content: '关键字4,关键字4,关键字4'
+      },
+      {
+        name: 'description',
+        content: '这是一段网页的描述4'
+      }
+    ]
+  },
+  account: {
+    title: '我是个人中心',
+    meta: [
+      {
+        name: 'keyWords',
+        content: '关键字5,关键字5,关键字5'
+      },
+      {
+        name: 'description',
+        content: '这是一段网页的描述5'
+      }
+    ]
+  },
+  detail: {
+    title: '我是详情界面',
+    meta: [
+      {
+        name: 'keyWords',
+        content: '关键字6,关键字6,关键字6'
+      },
+      {
+        name: 'description',
+        content: '这是一段网页的描述6'
+      }
+    ]
+  }
+}
